c0fab19704444f87d1b58fb41a29ce43b0ed5f99
anabelvitaliani <48593669+anabelvitaliani@users.noreply.github.com>
2019-05-12 18:18:14 -0300

    Add files via upload
    Menu y footer en un mismo archivo, responsive, para incluir en todas las pags

---
+++ Registro/css/menu.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../css/menu.css" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:700i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet"  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<title>Chocolatemia</title>
+</head>
+<body>
+  <header class="menu">
+    <div class="nombreymenu">
+      <h4><a class="nombre" href="home.php">Chocolatemia</a></h4>
+    </div>
+    <!--<div class="">
+    <img src="../Imagenes/img/menu2.png" alt="menu" class="hamburguesa">
+
+    </div>-->
+    <nav class="navegador">
+      <input type="checkbox" class="menu-hamb-check" id="menu-hamb">
+            <hr>
+      <ul>
+        <li><a href="home.php">Inicio</a></li>
+        <li><a href="register.php">Registrate</a></li>
+        <li><a href="login.php">Iniciar Sesión</a></li>
+        <li><a href="preguntas.php">Preguntas Frecuentes</a></li>
+        <li><a href="#contacto">Contactate con nosotros</a></li>
+      </ul>
+      <label for="menu-hamb"><img src="../Imagenes/img/menu2.png" alt="menu" class="hamburguesa"></label>
+    </nav>
+  </header>
+
+
+  <footer class="footer" id="contacto">
+    <a class="titulo-footer" href="home.php">Chocolatemia</a>
+
+    <!--<ul class="productos-footer">
+      <li><a href="#">BOMBONES</a></li>
+      <li><a href="#">TORTAS</a></li>
+      <li><a href="#">SIN TACC</a></li>
+      <li><a href="#">SIN LECHE</a></li>
+    </ul>-->
+
+    <ul class="contacto">
+      <li>Tel: 4863-1774</li>
+      <li>Mail: chocolatemia@gmail.com</li>
+    </ul>
+
+    <ul class="redes">
+      <li>
+        <a href="http://www.instagram.com" target="_blank">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="http://www.facebook.com" target="_blank">
+          <i class="fab fa-facebook-square"></i>
+        </a>
+      </li>
+      <li>
+        <a href="http://www.twitter.com" target="_blank">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+    </ul>
+
+
+  </footer>
+</body>
+</html>
