ee4413db8e82deecb30ceb6372b5bec7e8a21a4a
Romix1907 <48593733+Romix1907@users.noreply.github.com>
2019-05-14 20:11:07 -0300

    FAQ con css y responnsive OK

---
+++ PreguntasFrecuentes-Finalizado/preguntas.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <title>Chocolatemia</title>
+    </head>
+    <body>
+     <div class="contenedor">
+        <header class="header">
+          <header>
+            <div class="logoymenu">
+          <img src="" alt="" class="logo"><h1>CHOCOLATEMIA</h1><img src="img/menu2.png" alt="" class="menu"></div>
+            <ul>
+            <li><a href="#">TIENDA</a></li>
+            <li><a href="#">INICIAR SESION</a></li>
+            <li><a href="#">REGISTRATE</a></li>
+            <li><a href="#">CONTACTARNOS</a></li>
+          </ul>
+          </header>
+      </header>
+      <main class='preguntas'>
+        <br>
+        <br>
+        <h1>PREGUNTAS FRECUENTES</h1>
+        <br>
+        <article>
+          <b>Hola! ¿Hacen envíos?</b>
+          <p>Hacemos envíos a CABA y Gran Buenos Aires.</p>
+        </article>
+        <article>
+          <b>¿Venden chocolates sin T.A.C.C?</b>
+          <p>¡Hola! Tenemos una sección apta para celíacos. Podés verla en nuestra página!</p>
+        </article>
+        <article>
+          <b>¿Qué gustos tienen?</b>
+          <p>Tenemos chocolate blaco, amargo, semiamargo, con frutos secos (nuez, maní, almendras o combinado), rellenos de dulce de leche, frambuesa, menta, ¡entre muchos otros!</p>
+        </article>
+        <article>
+          <b>¿Qué costo tienen los envíos?</b>
+          <p>Si es a una dirección de CABA el envío es sin costo. Si no, se suma un 5% al valor de la compra.</p>
+        </article>
+        <article>
+          <b>¿Cuáles son los puntos para retirar las compras?</b>
+          <p>Podés retirar tu compra en nuestros locales (Ugarte 2732, Belgrano o Quintino Bocayúa 1542, Boedo) de 8 a 20hs.</p>
+        </article>
+        <article>
+          <b>¿Hacen tortas y postres?</b>
+          <p>Hacemos reposteríá únicamente a pedido.</p>
+        </article>
+        <article>
+          <b>Con cuánta anticipación tengo que realizar el pedido?</b>
+          <p>Los pedidos se retiran 48hs hábiles después de realizados en caso de ser chocolates que estén en nuestra página. Para otros sabores, formas o repostería se requieren 96hs hábiles. </p>
+        </article>
+        <article>
+          <b>Puedo reservar una compra para retirarlo más tarde en el local?</b>
+          <p>No hacemos reservas, las compras son por orden de llegada. Si sabés qué vas a necesitar con anticipación podés hacer un pedido con 48hs hábiles de anticipación.</p>
+        </article>
+      </main>
+    <br>
+  </html>
