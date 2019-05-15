81bd5fde66e1db9fce58f298c164723be16d2a91
Romix1907 <48593733+Romix1907@users.noreply.github.com>
2019-05-15 20:27:24 -0300

    index con php, css y responsive
    Solo falta hacer responsive la caja de las, card flip.

---
+++ Index-Finalizado/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC:700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <title>Chocolatemia</title>
+    </head>
+    <body>
+      <?php
+        require_once("funciones.php");
+      ?>
+     <div class="contenedor">
+        <header class="header">
+            <div class="logoymenu">
+          <img src="" alt="" class="logo"><h1>CHOCOLATEMIA</h1><img src="img/menu2.png" alt="" class="menu"></div>
+            <ul>
+            <li><a href="#">TIENDA</a></li>
+            <li><a href="#">INICIAR SESION</a></li>
+            <li><a href="#">REGISTRARSE</a></li>
+            <li><a href="#">FAQ</a></li>
+          </ul>
+          </header>
+          <main>
+            <h2 class="bienvenido">
+        			<?php
+        			//var_dump($_COOKIE);
+        			$emailLogueado=estaLogueado();
+        			if($emailLogueado!=""){
+        			$usuario=buscarUsuarioPorEmail($emailLogueado);
+        			//var_dump($usuario);
+
+        					if($usuario["genero"]=='f'){
+        						echo "¡Bienvenida, ".$usuario["nombre"]."!";
+        					} else if($usuario["genero"]=='m'){
+        						echo "¡Bienvenido, ".$usuario["nombre"]."!";
+        					} else if($usuario["genero"]=='nb'){
+        						echo "¡Bienvenide, ".$usuario["nombre"]."!";
+        					}
+                }	 else echo "¡Bienvenido!";
+
+        			?>
+      <section>
+        <br>
+        <div id="slider">
+    		<figure>
+    			<img src="img/pic01.jpg">
+    			<img src="img/pic4.jpg">
+    			<img src="img/pic11.jpg">
+    			<img src="img/pic12.jpg">
+    			<img src="img/pic01.jpg">
+    		</figure>
+    	</div>
+      <br>
+      <br>
+      <div class="wrap">
+    		<div class="tarjeta-wrap">
+    			<div class="tarjeta">
+    				<div class="adelante card1"></div>
+    				<div class="atras">
+    					<p>Alfajores artesanales recubiertos con el más exquisito chocolate, rellenos con dulce de leche.
+                <br>
+              <strong>Nada más argentino que un alfajor!</strong></p>
+    				</div>
+    			</div>
+    		</div>
+    		<div class="tarjeta-wrap">
+    			<div class="tarjeta">
+    				<div class="adelante card2"></div>
+    				<div class="atras">
+    					<p>Delicioso bocadito de chocolate relleno con un suave y tentador dulce de leche.
+                <br>
+                <strong>Chocolate y dulce de leche en su máxima expresión.</strong>
+              </p>
+            </div>
+    			</div>
+    		</div>
+    		<div class="tarjeta-wrap">
+    			<div class="tarjeta">
+    				<div class="adelante card3"></div>
+    				<div class="atras">
+    					<p>Disfrutá del verdadero encanto del licor con estos bombones de Café Irlandés.
+              <br>
+              <strong>Exquisita combinación de whisky y café.</strong>
+            </p>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+      <div class="container-primary">
+        <h3>- Como en Casa -</h3>
+        <p>Con más de 100 años en el mercado del chocolate te ofrecemos la calidad de la pastelería profesional e internacional y los sabores sublimes del más rico y puro chocolate.<br>
+           <strong><br> 100% artesanales</strong> asegurándote alimentos naturales sin agregados artificiales ni químicos.
+           <br>
+           <br>
+           Desde sus tiempos pioneros en que producía en forma artesanal cinco kilogramos por noche, evolucionamos hasta lo que somos hoy: una empresa totalmente argentina cuyo nivel industrial es comparable a los más avanzados del mundo. <br>
+           <br>
+
+        <cite>CHOCOLATEMIA</cite> te invita a su viaje de placer... ¿te animas?
+      </p>
+        </div>
+        </section>
+<br>
+<br>
+<br>
+
+
+    </main>
+
+    </body>
+    </html
